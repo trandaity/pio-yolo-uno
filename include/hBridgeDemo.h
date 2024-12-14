93fe3248f158c64149590131fe75862258dff62c
TyTran <trandaity151@gmail.com>
2024-12-14 13:23:45 +0700

    add demo functions for the h-bridge

---
+++ include/hBridgeDemo.h
@@ -0,0 +1,7 @@
+#include "M5UnitHbridge.h"
+
+extern M5UnitHbridge mtDriver;
+extern uint8_t fw_version;
+
+void getCurrentVoltage();
+void hBridgeDriverRun();
---
+++ src/hBridgeDemo.cpp
@@ -0,0 +1,41 @@
+#include "hBridgeDemo.h"
+
+M5UnitHbridge mtDriver;
+uint8_t fw_version = 0;
+
+void getCurrentVoltage() {
+  if (fw_version >= 2) {
+    Serial.printf(".2fA\r\n", mtDriver.getMotorCurrent());
+  }
+
+  Serial.printf(".2fV\r\n", mtDriver.getAnalogInput(_12bit) / 4095.0f * 3.3f / 0.09f);
+}
+
+void hBridgeDriverRun() {
+  while (1) {
+    mtDriver.setDriverDirection(HBRIDGE_FORWARD);
+    mtDriver.setDriverSpeed8Bits(127);
+    for (int i = 100; i > 0; i--) {
+        get_current_voltage();
+    }
+
+    mtDriver.setDriverDirection(HBRIDGE_STOP);
+    mtDriver.setDriverSpeed8Bits(0);
+    for (int i = 100; i > 0; i--) {
+         get_current_voltage();
+    }
+
+    mtDriver.setDriverDirection(HBRIDGE_BACKWARD);
+    mtDriver.setDriverSpeed8Bits(127);
+
+    for (int i = 100; i > 0; i--) {
+        get_current_voltage();
+    }
+
+    mtDriver.setDriverDirection(HBRIDGE_STOP);
+    mtDriver.setDriverSpeed8Bits(0);
+    for (int i = 100; i > 0; i--) {
+        get_current_voltage();
+    }
+  }
+}
