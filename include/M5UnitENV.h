12768243b4cef66542869c24719f96f2162ce04c
TyTran <trandaity151@gmail.com>
2025-04-24 18:47:26 +0700

    add libraries for the ENV unit

---
+++ include/M5UnitENV.h
@@ -0,0 +1,12 @@
+#if defined(M5_UNIT_UNIFIED_ENV_HPP)
+#error "DO NOT USE it at the same time as M5UnitUnified libraries"
+#endif
+
+#ifndef _M5_UNIT_ENV_H_
+#define _M5_UNIT_ENV_H_
+
+#include "Arduino.h"
+#include "SHT4X.h"
+#include "BMP280.h"
+
+#endif
